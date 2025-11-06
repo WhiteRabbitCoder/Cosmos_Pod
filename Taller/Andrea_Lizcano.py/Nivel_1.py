3d3912c6d4b0d318b3848391dd8ba8f3f4d6227e
andrealizcano <andrealizcano2023@gmail.com>
2025-11-06 13:56:38 -0500

    Hasta el momento termine el ejercicio 1 y 2 del nivel 1. los realice en clase

---
+++ Taller/Andrea_Lizcano.py/Nivel_1.py
@@ -0,0 +1,11 @@
+Nombre = input ("Por favor ingese su nombre: ")
+Edad = int(input("Por favor ingrese su edad: "))
+print (f"Hola",Nombre, "Tienes", Edad, "Años.")
+
+
+print("_________________Suma__________________")
+
+Numero1 = input ("Ingrese por favor su primer numero de la suma: ")
+Numero2 = input ("Ingrese por favor su segundo numero de la suma: ")
+print (Numero1 + Numero2)
+
