5b3f695fc7488cf5f1a6fb208c16cb52eb1ca159
JuanJoseRiwi <guarinmedinajj@gmail.com>
2025-11-07 13:39:51 -0500

    ejercicios hasta el nivel5

---
+++ Semana1/NIVEL4/eliminar_duplicado.py
@@ -0,0 +1,4 @@
+#Aqui se define la variable y se le hace un set y una lista para que elimine los duplicados y se imprime la lista sin repetidos
+lista = ["12", "13", "2", "12", "13"]
+lista_sin_repetidos = list(set(lista))
+print(lista_sin_repetidos)
