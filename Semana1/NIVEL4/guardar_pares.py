11f50b44b508b138b4a8e8d402663dcf54ddb33c
JuanJoseRiwi <guarinmedinajj@gmail.com>
2025-11-07 12:41:24 -0500

    ejercicio de guardar pares

---
+++ Semana1/NIVEL4/guardar_pares.py
@@ -0,0 +1,9 @@
+numero=int(input("Ingrese un numero"))
+
+if numero % 2==0:
+    print("El numero es par") 
+    lista=[]
+    lista.append(numero)
+    print(lista)
+else:
+    print("El numero es impar")
