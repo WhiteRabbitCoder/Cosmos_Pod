178a1443db5f9109254e281ec73430d35a225c45
JuanJoseRiwi <guarinmedinajj@gmail.com>
2025-11-09 17:59:58 -0500

    Carrito de compras

---
+++ Semana1/NIVEL5/carrito_compras.py
@@ -0,0 +1,27 @@
+productos = {"manzana": 1000, "banano":800, "pan": 2000, "leche": 4000}
+
+# Lista vacía para el carrito
+carrito = []
+
+while True:
+    print("\nProductos disponibles:")
+    for p, precio in productos.items():
+        print(f"- {p}: ${precio}")
+
+    eleccion = input("\n¿Qué producto quieres agregar? (o escribe 'salir' para terminar): ").lower()
+
+    if eleccion == "salir":
+        break
+    elif eleccion in productos:
+        carrito.append(eleccion)
+        print(f" {eleccion} agregado al carrito.")
+    else:
+        print("Producto no encontrado, intenta de nuevo.")
+
+# Calcular el total
+total = 0
+for item in carrito:
+    total += productos[item]
+
+print("\nTu carrito:", carrito)
+print(f"Total a pagar: ${total:.2f}")
